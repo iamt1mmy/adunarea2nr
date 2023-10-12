dd48a815347ed4a571ad81353d35d30f7e122b25
iamt1mmy <146391141+iamt1mmy@users.noreply.github.com>
2023-10-12 22:12:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace adunare2nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, s;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            s = x + y;
+
+            Console.WriteLine(s);
+        }
+    }
+}
